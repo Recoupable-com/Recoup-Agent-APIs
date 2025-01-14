d66bc5a7fe976861280f47ff9ecbaed247a4219c
Va~~~dym <tech.eng.me@gmail.com>
2025-01-14 05:43:59 -0800

    I add dev log

--- agents/getTikTokAnalysis.js
+++ agents/getTikTokAnalysis.js
@@ -36,6 +36,7 @@ const getTikTokAnalysis = async (
       STEP_OF_ANALYSIS.PROFILE,
     );
     const accountData = await getProfile(profileDatasetId);
+    console.log("ZIAD", accountData);
     const profile = accountData?.profile?.[0];
     const videoUrls = accountData?.videoUrls;
     const avatar = await uploadPfpToIpfs(profile.avatar);

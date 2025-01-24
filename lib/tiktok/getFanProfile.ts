71496c02836db510a8820493b2deb2243d442828
Va~~~dym <tech.eng.me@gmail.com>
2025-01-24 08:05:08 -0800

    I complete tiktok profile scraping

--- lib/tiktok/getFanProfile.ts
+++ lib/tiktok/getFanProfile.ts
@@ -7,15 +7,30 @@ const getFanProfile = async (handle: string) => {
     const profilePageUrl = `https://tiktok.com/@${handle}`;
     const response = await axios.get(profilePageUrl);
     let $ = cheerio.load(response.data);
+    const scripts = $("script");
 
-    const followerCount = $("[title='Followers']").html();
-    const bio = $("[data-e2e='user-bio']").html();
-    const avatar = $("[class*='ImgAvatar']").html();
+    let userInfo: any = null;
+    scripts.each((_, element) => {
+      try {
+        const scriptContent = $(element).html();
+        const scriptSrc = $(element).attr("src");
 
-    // const email = extracMails(bio);
+        if (!scriptSrc && scriptContent) {
+          const metadata = JSON.parse(scriptContent);
+          if (Object.keys(metadata)[0] === "__DEFAULT_SCOPE__")
+            userInfo = metadata["__DEFAULT_SCOPE__"]["webapp.user-detail"];
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    });
 
     return {
-      data: response.data,
+      handle: userInfo?.user?.uniqueId || handle,
+      bio: userInfo?.user?.signature || "",
+      avatar: userInfo?.user?.avatarThumb || "",
+      followerCount: userInfo?.stats?.followerCount,
+      email: extracMails(userInfo?.user?.signature || ""),
     };
   } catch (error) {
     console.error(error);

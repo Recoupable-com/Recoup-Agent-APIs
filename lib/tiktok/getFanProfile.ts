6f2c48f6232530c084b453e81fd8b3a9a0634204
Va~~~dym <tech.eng.me@gmail.com>
2025-01-24 06:47:27 -0800

    I add tiktok profile getting api

---
+++ lib/tiktok/getFanProfile.ts
@@ -0,0 +1,30 @@
+import * as cheerio from "cheerio";
+import axios from "axios";
+import extracMails from "../extracMails";
+
+const getFanProfile = async (handle: string) => {
+  try {
+    const profilePageUrl = `https://tiktok.com/${handle}`;
+    const response = await axios.get(profilePageUrl);
+    let $ = cheerio.load(response.data);
+
+    const followerCount = $("[title='Followers']").text();
+    const bio = $("[data-e2e='user-bio']").text();
+    const avatar = $("[class*='ImgAvatar']").first().text();
+
+    const email = extracMails(bio);
+
+    return {
+      handle,
+      followerCount,
+      bio,
+      avatar,
+      email,
+    };
+  } catch (error) {
+    console.error(error);
+    return "";
+  }
+};
+
+export default getFanProfile;

3687409babc300c81ede59b9adc1bbba4e115ee9
Va~~~dym <tech.eng.me@gmail.com>
2025-01-24 06:58:28 -0800

    I update get request

--- lib/tiktok/getFanProfile.ts
+++ lib/tiktok/getFanProfile.ts
@@ -1,13 +1,24 @@
 import * as cheerio from "cheerio";
-import axios from "axios";
 import extracMails from "../extracMails";
+import { Stagehand } from "@browserbasehq/stagehand";
 
 const getFanProfile = async (handle: string) => {
   try {
     const profilePageUrl = `https://tiktok.com/${handle}`;
-    const response = await axios.get(profilePageUrl);
-    console.log("ZIAD", response.data);
-    let $ = cheerio.load(response.data);
+    const stagehand = new Stagehand({
+      env: "LOCAL",
+      verbose: 1,
+      debugDom: true,
+      enableCaching: false,
+      headless: true,
+      modelName: "gpt-4o-mini",
+    });
+
+    await stagehand.init();
+    await stagehand.page.goto(profilePageUrl);
+    const pageContent = await stagehand.page.content();
+    console.log("ZIAD", pageContent);
+    let $ = cheerio.load(pageContent);
 
     const followerCount = $("[title='Followers']").text();
     const bio = $("[data-e2e='user-bio']").text();

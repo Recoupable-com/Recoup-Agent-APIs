31d5fcfc672b8cd48ca71e6161bdedec27346371
Va~~~dym <tech.eng.me@gmail.com>
2025-01-26 23:17:39 -0800

    I add fans profiles

--- lib/supabase/saveFansProfiles.ts
+++ lib/supabase/saveFansProfiles.ts
@@ -4,8 +4,7 @@ const saveFansProfiles = async (profiles: any) => {
   const { data } = await supabase
     .from("fans_segments")
     .insert(profiles)
-    .select("*")
-    .single();
+    .select("*");
 
   return data;
 };

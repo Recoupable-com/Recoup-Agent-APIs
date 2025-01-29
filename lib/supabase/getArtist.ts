c4ebcbbc6a404a8ceb20030aa6c6b214368666a2
Va~~~dym <tech.eng.me@gmail.com>
2025-01-29 10:45:55 -0800

    I fix getArtist

--- lib/supabase/getArtist.ts
+++ lib/supabase/getArtist.ts
@@ -2,8 +2,8 @@ import supabase from "./serverClient.js";
 
 const getArtist = async (artist_id: string | null) => {
   const { data: account } = await supabase
-    .from("account, account_info(*), account_socials(*)")
-    .select("*")
+    .from("accounts")
+    .select("*, account_info(*), account_socials(*)")
     .eq("id", artist_id)
     .single();
   return {

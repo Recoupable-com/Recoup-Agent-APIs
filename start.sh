c68b7ab2877e8b7321b4c9aefc12ec7cb2f73aae
Sweets Sweetman <sweetmantech@gmail.com>
2025-01-31 19:02:11 -0300

    remove start script.

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PATH="/usr/local/bin:/usr/bin:/bin:/root/.bun/bin:$PATH"
-cd ~/recoup-agent-apis
-bun x tsc && nodemon --experimental-specifier-resolution=node --no-warnings ./dist/app.js 

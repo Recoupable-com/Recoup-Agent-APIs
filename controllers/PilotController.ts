e953fff68e57d186e93061e373b52f07a5de33c0
Va~~~dym <tech.eng.me@gmail.com>
2025-02-01 23:30:47 -0800

    I fix handles

--- controllers/PilotController.ts
+++ controllers/PilotController.ts
@@ -23,13 +23,29 @@ export const run_agent = async (req: Request, res: Response) => {
     if (!agent?.id || !handles) return;
 
     if (isWrapped || type === Funnel_Type.TIKTOK)
-      runTikTokAgent(agent.id, handles["tiktok"], artistId as string);
+      runTikTokAgent(
+        agent.id,
+        handles["tiktok"].replaceAll("@", ""),
+        artistId as string,
+      );
     if (isWrapped || type === Funnel_Type.TWITTER)
-      runTwitterAgent(agent.id, handles["twitter"], artistId as string);
+      runTwitterAgent(
+        agent.id,
+        handles["twitter"].replaceAll("@", ""),
+        artistId as string,
+      );
     if (isWrapped || type === Funnel_Type.INSTAGRAM)
-      runInstagramAgent(agent.id, handles["instagram"], artistId as string);
+      runInstagramAgent(
+        agent.id,
+        handles["instagram"].replaceAll("@", ""),
+        artistId as string,
+      );
     if (isWrapped || type === Funnel_Type.SPOTIFY)
-      runSpotifyAgent(agent.id, handles["spotify"], artistId as string);
+      runSpotifyAgent(
+        agent.id,
+        handles["spotify"].replaceAll("@", ""),
+        artistId as string,
+      );
     return res.status(200).json({ agentId: agent.id });
   } catch (error) {
     console.error("❌ [PilotController] Error in run_agent:", error);

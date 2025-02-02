96a08264b625d5ea7f0b083e715c7ae00df896a6
Va~~~dym <tech.eng.me@gmail.com>
2025-02-02 02:28:40 -0800

    fix existing social

--- lib/supabase/updateSocial.ts
+++ lib/supabase/updateSocial.ts
@@ -13,20 +13,25 @@ const updateSocial = async (
       .eq("id", social_id)
       .single();
     if (existing_social) {
+      const { data: existing_socials } = await supabase
+        .from("socials")
+        .select("*")
+        .eq("profile_url", socialdata.profile_url);
+      console.log("ZIAD existing_socials", existing_socials);
       console.log("ZIAD OKAY", {
         ...existing_social,
         ...socialdata,
-      })
+      });
       const { error } = await supabase
-      .from("socials")
-      .update({
-        ...existing_social,
-        ...socialdata,
-      })
-      .eq("id", existing_social.id)
-      .select("*")
-      .single();
-      console.log("ZIAD", error)
+        .from("socials")
+        .update({
+          ...existing_social,
+          ...socialdata,
+        })
+        .eq("id", existing_social.id)
+        .select("*")
+        .single();
+      console.log("ZIAD", error);
       return { error: null };
     }
     return { error: null };

e1cce839097b818fc7e0d648aac026f292e200c0
Va~~~dym <tech.eng.me@gmail.com>
2025-02-03 06:26:12 -0800

    I add dev log

--- lib/apify/getDataset.ts
+++ lib/apify/getDataset.ts
@@ -1,18 +1,23 @@
 import { APIFY_TOKEN } from "../consts";
 
 const getDataset = async (datasetId: string) => {
-  const response = await fetch(
-    `https://api.apify.com/v2/datasets/${datasetId}/items?token=${APIFY_TOKEN}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
+  try {
+    const response = await fetch(
+      `https://api.apify.com/v2/datasets/${datasetId}/items?token=${APIFY_TOKEN}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       },
-    },
-  );
-  const data = await response.json();
-
-  return data;
+    );
+    const data = await response.json();
+  
+    return data;
+  } catch(error) {
+    console.error(error)
+    return null
+  }
 };
 
 export default getDataset;

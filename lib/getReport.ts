08e0045eccdf527cf67b098848adf5ed0a7649d7
Va~~~dym <tech.eng.me@gmail.com>
2025-02-02 22:54:49 -0800

    I update get report api

--- lib/getReport.ts
+++ lib/getReport.ts
@@ -1,15 +1,21 @@
 import getChatCompletions from "./getChatCompletions";
 import { instructions } from "../lib/instructions";
 import {
+  AI_MODEL,
   FULL_REPORT_NOTE,
   HTML_RESPONSE_FORMAT_INSTRUCTIONS,
   REPORT_NEXT_STEP_NOTE,
 } from "../lib/consts";
+import OpenAI from "openai";
 
 const getReport = async (context: any) => {
   try {
-    const reportContent = await getChatCompletions(
-      [
+    const openai = new OpenAI();
+    const response = await openai.chat.completions.create({
+      model: AI_MODEL,
+      max_tokens: 2222,
+      temperature: 0.7,
+      messages: [
         {
           role: "user",
           content: `
@@ -22,27 +28,23 @@ const getReport = async (context: any) => {
               ${HTML_RESPONSE_FORMAT_INSTRUCTIONS}
               NOTE: ${FULL_REPORT_NOTE}`,
         },
+        {
+          role: "user",
+          content: `Context: ${JSON.stringify(context)}`,
+        },
+        {
+          role: "system",
+          content: `${instructions.get_segments_report_next_step}
+                ${HTML_RESPONSE_FORMAT_INSTRUCTIONS}
+                NOTE: ${REPORT_NEXT_STEP_NOTE}`,
+        },
       ],
-      2222,
-    );
+    });
 
-    const nextSteps = await getChatCompletions([
-      {
-        role: "user",
-        content: `Context: ${JSON.stringify(context)}`,
-      },
-      {
-        role: "system",
-        content: `${instructions.get_segments_report_next_step}
-              ${HTML_RESPONSE_FORMAT_INSTRUCTIONS}
-              NOTE: ${REPORT_NEXT_STEP_NOTE}`,
-      },
-      2222,
-    ]);
-
+    console.log("ZIAD HERE", response);
     return {
-      nextSteps,
-      reportContent,
+      nextSteps: "",
+      reportContent: "",
     };
   } catch (error) {
     return {

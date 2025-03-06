fccb8f4159b06a5517bf38fcf1b9e3d975338070
Sweets Sweetman <sweetmantech@gmail.com>
2025-03-06 16:33:40 -0300

    new Twitter Scraper follows BaseScraper.

---
+++ lib/scraping/platforms/twitter/TwitterScraper.ts
@@ -0,0 +1,62 @@
+import { BaseScraper } from "../../BaseScraper";
+import { ScrapedProfile, ScrapedPost, ScrapedComment } from "../../types";
+import { Database } from "../../../../types/database.types";
+import getProfile from "../../../twitter/getProfile";
+import getVideoComments from "../../../tiktok/getVideoComments";
+import { Scraper, Tweet } from "agent-twitter-client";
+import { getAllTweets } from "../../../twitter/getAllTweets";
+
+const scraper = new Scraper();
+
+export class TwitterScraper extends BaseScraper {
+  async scrapeProfile(handle: string): Promise<ScrapedProfile> {
+    try {
+      const { profile } = await getProfile(scraper, handle);
+
+      if (!profile) {
+        throw new Error("Profile not found");
+      }
+
+      return {
+        username: handle,
+        profile_url: `https://twitter.com/${handle}`,
+        avatar: profile.avatar || undefined,
+        followerCount: profile.followerCount || undefined,
+        description: profile.bio || undefined,
+      };
+    } catch (error) {
+      return this.handleError(error, "TikTokScraper.scrapeProfile");
+    }
+  }
+
+  async scrapePosts(handle: string): Promise<ScrapedPost[]> {
+    try {
+      const allTweets = await getAllTweets(scraper, handle);
+      console.log("allTweets", allTweets[0]);
+      if (!allTweets?.length) {
+        return [];
+      }
+
+      return allTweets.map((tweet: Tweet) => ({
+        post_url: tweet.permanentUrl || "",
+        platform: "twitter" as Database["public"]["Enums"]["social_type"],
+        created_at: tweet.timestamp
+          ? new Date(tweet.timestamp).toISOString()
+          : new Date().toISOString(),
+        media_type: "post",
+        content: tweet.text || "",
+        media_url: tweet.photos[0]?.url || tweet.videos[0]?.url || "",
+      }));
+    } catch (error) {
+      return this.handleError(error, "TikTokScraper.scrapePosts");
+    }
+  }
+
+  async scrapeComments(postUrls: string[]): Promise<ScrapedComment[]> {
+    try {
+      return [];
+    } catch (error) {
+      return this.handleError(error, "TikTokScraper.scrapeComments");
+    }
+  }
+}

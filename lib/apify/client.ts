c3200713cac0bce030e99634bf36c1d21d3a739f
Sweets Sweetman <sweetmantech@gmail.com>
2025-11-10 20:10:09 -0500

    Fail fast when APIFY_TOKEN is missing

--- lib/apify/client.ts
+++ lib/apify/client.ts
@@ -1,6 +1,10 @@
 import { ApifyClient } from "apify-client";
 import { APIFY_TOKEN } from "../consts";
 
+if (!APIFY_TOKEN) {
+  throw new Error("Missing APIFY_TOKEN environment variable");
+}
+
 export const apifyClient = new ApifyClient({
   token: APIFY_TOKEN,
 });

9e88c0171062664d4e76e4f36ab7cd94724c8b73
Sweets Sweetman <sweetmantech@gmail.com>
2025-11-13 16:11:27 -0500

    Webhook - Apify update profile - facebook

--- lib/apify/webhooks/socialProfileWebhook/getSocialProfileParser.ts
+++ lib/apify/webhooks/socialProfileWebhook/getSocialProfileParser.ts
@@ -1,4 +1,5 @@
 import type { SocialProfileParser } from "./types";
+import { facebookProfileParser } from "./facebookProfileParser";
 import { instagramProfileParser } from "./instagramProfileParser";
 import { tiktokProfileParser } from "./tiktokProfileParser";
 import { threadsProfileParser } from "./threadsProfileParser";
@@ -10,6 +11,7 @@ export const TIKTOK_PROFILE_SCRAPER_ACTOR_ID = "GdWCkxBtKWOsKjdch" as const;
 export const YOUTUBE_PROFILE_SCRAPER_ACTOR_ID = "h7sDV53CddomktSi5" as const;
 export const THREADS_PROFILE_SCRAPER_ACTOR_ID = "kJdK90pa2hhYYrCK5" as const;
 export const TWITTER_PROFILE_SCRAPER_ACTOR_ID = "nfp1fpt5gUlBwPcor" as const;
+export const FACEBOOK_PROFILE_SCRAPER_ACTOR_ID = "4Hv5RhChiaDk6iwad" as const;
 
 const SOCIAL_PROFILE_PARSERS: Record<string, SocialProfileParser> = {
   [INSTAGRAM_PROFILE_SCRAPER_ACTOR_ID]: instagramProfileParser,
@@ -17,6 +19,7 @@ const SOCIAL_PROFILE_PARSERS: Record<string, SocialProfileParser> = {
   [YOUTUBE_PROFILE_SCRAPER_ACTOR_ID]: youtubeProfileParser,
   [THREADS_PROFILE_SCRAPER_ACTOR_ID]: threadsProfileParser,
   [TWITTER_PROFILE_SCRAPER_ACTOR_ID]: twitterProfileParser,
+  [FACEBOOK_PROFILE_SCRAPER_ACTOR_ID]: facebookProfileParser,
 };
 
 export const getSocialProfileParser = (
